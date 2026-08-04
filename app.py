7edface020890257d6ad8bb224c4b41037ccda5c
kai wen tan <smark.work.me@gmail.com>
2026-08-04 12:25:18 +0900

    change download method

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 import os
-from flask import Flask, jsonify, render_template_string, send_from_directory, request
+from flask import Flask, jsonify, render_template_string, request
 
 app = Flask(__name__)
-PUBLIC_DIR = os.path.join(os.getcwd(), 'public')
-if not os.path.exists(PUBLIC_DIR):
-    os.makedirs(PUBLIC_DIR)
 
 @app.route('/')
 def index():
     with open('index.html', 'r') as f:
         return render_template_string(f.read())
-
-# Route to serve the single pics.rar file
-@app.route('/download/rar')
-def download_rar():
-    return send_from_directory(PUBLIC_DIR, 'pics.rar', as_attachment=True)
 
 # Route to check the verification code
 @app.route('/verify', methods=['POST'])
